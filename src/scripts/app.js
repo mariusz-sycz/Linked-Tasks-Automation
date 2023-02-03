1fe985068ba88ca74ab4a5a644bf8930efa74897
Mariusz Sycz <mariusz.sycz@vistex.com>
2023-02-03 09:50:16 +0100

    Add main files and configurations

---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+define(["TFS/WorkItemTracking/Services", "TFS/WorkItemTracking/RestClient", "TFS/Work/RestClient", "q", "VSS/Controls", "VSS/Controls/StatusIndicator", "VSS/Controls/Dialogs"],
+    function (_WorkItemServices, _WorkItemRestClient, workRestClient, Q, Controls, StatusIndicator, Dialogs) {
+
+        var ctx = null;
+
+    });
