87932a476ab72ba01d725b8652a88f02cfcc2704
Mariusz Sycz <mariusz.sycz@vistex.com>
2026-08-08 15:42:55 +0200

    Stage build output to top-level build/ instead of in-source src/

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -7,57 +7,68 @@
                 stderr: true
             },
             package_dev: {
-                command: "tfx extension create --rev-version --manifests vss-extension.json --overrides-file configs/dev.json --output-path ../dist" ,
+                command: "tfx extension create --root ../build --rev-version --manifests vss-extension.json --overrides-file configs/dev.json --output-path ../dist" ,
                 stdout: true,
                 stderr: true
             },
             package_release: {
-                command: "tfx extension create  --manifests vss-extension.json --overrides-file configs/release.json --output-path ../dist",
+                command: "tfx extension create --root ../build --manifests vss-extension.json --overrides-file configs/release.json --output-path ../dist",
                 stdout: true,
                 stderr: true
             },
             publish_dev: {
-                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --manifests vss-extension.json --overrides-file configs/dev.json --output-path ../dist",
+                command: "tfx extension publish --root ../build --service-url https://marketplace.visualstudio.com --manifests vss-extension.json --overrides-file configs/dev.json --output-path ../dist",
                 stdout: true,
                 stderr: true
             },
             publish_release: {
-                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --manifests vss-extension.json --overrides-file configs/release.json --output-path ../dist",
+                command: "tfx extension publish --root ../build --service-url https://marketplace.visualstudio.com --manifests vss-extension.json --overrides-file configs/release.json --output-path ../dist",
                 stdout: true,
                 stderr: true
             },
             serve: {
-                command: "http-server . -S -C certs/cert.pem -K certs/key.pem -p 5501 -a localhost -c-1",
+                command: "http-server ../build -S -C certs/cert.pem -K certs/key.pem -p 5501 -a localhost -c-1",
                 stdout: true,
                 stderr: true
             }
         },
         copy: {
-            scripts: {
-                files: [{
-                    expand: true, 
-                    flatten: true, 
-                    src: ["node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js"], 
-                    dest: "lib",
-                    filter: "isFile" 
-                }]
+            static: {
+                files: [
+                    { src: "toolbar.html", dest: "../build/toolbar.html" },
+                    { src: "vss-extension.json", dest: "../build/vss-extension.json" },
+                    { src: "overview.md", dest: "../build/overview.md" },
+                    { expand: true, cwd: "img", src: ["**"], dest: "../build/img" },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ["node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js"],
+                        dest: "../build/lib",
+                        filter: "isFile"
+                    }
+                ]
             }
         },
 
-        clean: ["../dist/*.vsix"],
-
-        
+        clean: {
+            options: {
+                force: true
+            },
+            vsix: ["../dist/*.vsix"],
+            build: ["../build"]
+        }
     });
     
     grunt.loadNpmTasks("grunt-exec");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("package-dev", ["exec:tsc", "exec:package_dev"]);
-    grunt.registerTask("package-release", ["exec:tsc", "exec:package_release"]);
+    grunt.registerTask("build", ["clean:build", "exec:tsc", "copy:static"]);
+    grunt.registerTask("package-dev", ["build", "exec:package_dev"]);
+    grunt.registerTask("package-release", ["build", "exec:package_release"]);
     grunt.registerTask("publish-dev", ["package-dev", "exec:publish_dev"]);        
     grunt.registerTask("publish-release", ["package-release", "exec:publish_release"]);        
-    grunt.registerTask("serve", ["exec:tsc", "exec:serve"]);
+    grunt.registerTask("serve", ["build", "exec:serve"]);
     
     grunt.registerTask("default", ["package-dev"]);
 };
